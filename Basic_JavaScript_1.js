d2e5e770e29f54ef4cb14e5300b5ffdb1e266270
solicyrus <cyrusamiri99@gmail.com>
2025-04-02 16:16:43 -0700

    Added font code to Basic_HTML_and_JavaScript_1

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+ alert("Hello, World!");
+ function My_First_Function() {
+    var string = "kiss me, I'm Irish!";
+    var result = string.fontcolor("green");
+    document.getElementById("Irish").innerHTML =
+    result;
+ }
+ ("It has in JavaScript")
